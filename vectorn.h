245f70e2c056c6fe9c26efd490ab0fe73ca08485
Arthur Baldoqui <arthur.b.bergamo@gmail.com>
2024-10-25 20:59:18 -0300

    implemented vectorn

--- vectorn.h
+++ vectorn.h
@@ -1,22 +1,103 @@
 #ifndef VECTORN_H
 #define VECTORN_H
 
-template <typename T> class Vector2{
-    T x;
-    T y;
+template <typename T>
+class Vector2 {
+public:
+    T x, y;
+
+    // Construtor padrão
+    Vector2() : x(0), y(0) {}
+
+    // Construtor parametrizado
+    Vector2(T x, T y) : x(x), y(y) {}
+
+    // Operadores
+    Vector2<T> operator+(const Vector2<T>& other) const {
+        return Vector2<T>(x + other.x, y + other.y);
+    }
+
+    Vector2<T> operator-(const Vector2<T>& other) const {
+        return Vector2<T>(x - other.x, y - other.y);
+    }
+
+    Vector2<T>& operator+=(const Vector2<T>& other) {
+        x += other.x;
+        y += other.y;
+        return *this;
+    }
+
+    Vector2<T>& operator-=(const Vector2<T>& other) {
+        x -= other.x;
+        y -= other.y;
+        return *this;
+    }
 };
 
-template <typename T> class Vector3{
-    T x;
-    T y;
-    T z;
+template <typename T>
+class Vector3 {
+public:
+    T x, y, z;
+
+    Vector3() : x(0), y(0), z(0) {}
+
+    Vector3(T x, T y, T z) : x(x), y(y), z(z) {}
+
+    Vector3<T> operator+(const Vector3<T>& other) const {
+        return Vector3<T>(x + other.x, y + other.y, z + other.z);
+    }
+
+    Vector3<T> operator-(const Vector3<T>& other) const {
+        return Vector3<T>(x - other.x, y - other.y, z - other.z);
+    }
+
+    Vector3<T>& operator+=(const Vector3<T>& other) {
+        x += other.x;
+        y += other.y;
+        z += other.z;
+        return *this;
+    }
+
+    Vector3<T>& operator-=(const Vector3<T>& other) {
+        x -= other.x;
+        y -= other.y;
+        z -= other.z;
+        return *this;
+    }
 };
 
-template <typename T> class Vector4{
-    T x;
-    T y;
-    T z;
-    T w;
+template <typename T>
+class Vector4 {
+public:
+    T x, y, z, w;
+
+    Vector4() : x(0), y(0), z(0), w(0) {}
+
+    Vector4(T x, T y, T z, T w) : x(x), y(y), z(z), w(w) {}
+
+    Vector4<T> operator+(const Vector4<T>& other) const {
+        return Vector4<T>(x + other.x, y + other.y, z + other.z, w + other.w);
+    }
+
+    Vector4<T> operator-(const Vector4<T>& other) const {
+        return Vector4<T>(x - other.x, y - other.y, z - other.z, w - other.w);
+    }
+
+    Vector4<T>& operator+=(const Vector4<T>& other) {
+        x += other.x;
+        y += other.y;
+        z += other.z;
+        w += other.w;
+        return *this;
+    }
+
+    Vector4<T>& operator-=(const Vector4<T>& other) {
+        x -= other.x;
+        y -= other.y;
+        z -= other.z;
+        w -= other.w;
+        return *this;
+    }
 };
 
 #endif // VECTORN_H
